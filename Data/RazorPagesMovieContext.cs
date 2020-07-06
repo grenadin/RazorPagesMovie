27c2b03a6fa27c4d59c428dfd11b073bb1de0e3d
grenadin <harirak@hotmail.co.th>
2020-07-07 00:14:40 +0700

    Add project files.

---
+++ Data/RazorPagesMovieContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext //gen by efcofe use scaffold
+    {
+
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; } 
+        //DBSet Class used to query and save instance of TEntity. it return result from database 
+        //This is Properties
+        //It can cut RazorPagesMovie.Models. from this to show only DbSet<Movie>.
+        //It think like a folder.
+
+        
+    }
+}
---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;// Actually, It look like Mvc
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+    public class IndexModel : PageModel //derived from PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;//Why want this field
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context) //inject 
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync() 
+        {
+            Movie = await _context.Movie.ToListAsync(); //call list of Movie in db if it has ant movie in the list
+        }//This Method OnGet or OnGetAsync called. It's not necessary to return value 
+    }
+}
